0cd3b424b9e00ce4857b8183491b414daaa4137a
bchutchison <benhutchison85@gmail.com>
2019-08-06 16:50:00 +0100

    drop down list with currency name

--- src/app.js
+++ src/app.js
@@ -3,17 +3,20 @@ import Vue from 'vue';
 document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
-
     data: {
+      currenciesArray: []
+    },
 
-
-
+    mounted(){
+      this.getCurrencies()
     },
 
     methods: {
-
-
-
+      getCurrencies: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(currency => this.currenciesArray = currency.rates)
+      }
     },
 
   })
